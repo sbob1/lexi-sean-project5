145160850416e9f5772fab846ebc3b848e1996bd
Sean Roberts <seanroberts@Seans-MacBook-Pro-2.local>
2022-12-12 21:21:17 -0700

    process done

--- src/process/process.cpp
+++ src/process/process.cpp
@@ -11,7 +11,26 @@ using namespace std;
 
 Process* Process::read_from_input(std::istream& in) {
     // TODO
-    return nullptr;
+    //return nullptr;
+
+    std::vector<Page*> pages;
+
+    size_t bytes_num = 0;
+    
+    while(!in.eof()){
+        Page* this_page = Page::read_from_input(in);
+
+        pages.push_back(this_page);
+
+        bytes_num += in.gcount();
+
+
+    }
+
+    Process* return_process = new Process(num_bytes, pages);
+
+    return return_process;
+
 }
 
 
@@ -25,19 +44,25 @@ size_t Process::size() const
 bool Process::is_valid_page(size_t index) const
 {
     // TODO
+    if(pages.size() > index){
+        return true;
+    }
+    else{
     return false;
+    }
 }
 
 
 size_t Process::get_rss() const
 {
     // TODO
-    return 0;
+    return page_table.get_present_page_count();
 }
 
 
 double Process::get_fault_percent() const
 {
     // TODO
-    return 0.0;
+    if (memory_accesses == 0) return 0;
+    else return page_faults * 100.0 / memory_accesses;
 }
